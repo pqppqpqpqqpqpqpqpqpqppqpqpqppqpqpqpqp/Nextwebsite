3f39f3eb43ce79ecf02cdb7d4eee53c6bfc5ac8c
pqppqpqpqqpqpqpqpqpqppqpqpqppqpqpqpqp <107128224+pqppqpqpqqpqpqpqpqpqppqpqpqppqpqpqpqp@users.noreply.github.com>
2022-06-11 14:32:10 +0530

    signup update

--- pages/signup.js
+++ pages/signup.js
@@ -34,7 +34,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     const data = { name, email, password }
-    let res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/signup`, {
+    let res = await fetch(`/api/signup`, {
       method: 'POST', // or 'PUT'
       headers: {
         'Content-Type': 'application/json',
